0ee01a02180e2ddb921e80e2fb304ab8506966ae
jon-vy <jon_vy@mail.ru>
2020-01-31 18:57:52 +0300

    реализовал ресайз billboard

--- app/js/main.js
+++ app/js/main.js
@@ -28,10 +28,13 @@
     h = c.height = window.innerHeight;
   })
 })()
+
+
+
 /* Функция для перещёта размера пентакля */
 function pentacle() {
-  var width_Blok_Left = document.getElementById('blok-left').offsetWidth;/* ширина левого блока */
-  var width_img_pentacle = document.getElementById('container-pentacle').offsetWidth;/* ширина блока с пентаклями */
+  var width_Blok_Left = document.getElementById('blok-left').offsetWidth;/*получить ширину левого блока */
+  var width_img_pentacle = document.getElementById('container-pentacle').offsetWidth;/* получить ширину блока с пентаклями */
   var distance = width_img_pentacle / 2; /* расстояние между пентаклями */
   var left_img_pentacle = (width_Blok_Left - width_img_pentacle) / 2;/* высчитываю сдвиг вправо */
   document.getElementById('container-pentacle').style.bottom = width_img_pentacle + 'px';/* сдвигаю в верх блок с пентаклями */
@@ -44,23 +47,97 @@ function pentacle() {
   document.getElementById('bottom').style.transform = 'rotateX(-90deg)' + 'translateZ(' + distance + 'px )';
   document.getElementById('container-pentacle').style.transformOrigin = distance + 'px ' + distance + 'px';
 }
+
 pentacle();
+
 var container_pentacle = document.getElementById('container-pentacle'),
 handler = (function () {
   var timer;
-  var delay = 1000; /* чтобы сильно не грузить браузер обработчик срабатывает с задержкой 1000мс после ресайза окна */
-  return function () {
+  var delay = 1000; /*чтобы сильно не грузить браузер обработчик срабатывает с задержкой 1000мс после ресайза окна */
+ 
+   return function () {
+    
     if (timer) { 
       clearTimeout(timer); 
     }
     timer = setTimeout(function () {
       if (container_pentacle.offsetWidth != container_pentacle.widthBefor) {
         pentacle();
+        billboardF();
       }
       container_pentacle.widthBefor = container_pentacle.offsetWidth;
     }, delay);
   };
 })();
 container_pentacle.widthBefor = container_pentacle.offsetWidth;  /* запоминает исходную ширину блока */
-window.onresize = handler;  /* срабатывает при ресайзе окна браузера */
+window.onresize = handler;   /*срабатывает при ресайзе окна браузера */
+
+
+
+
+
+/* кнопки с billboard */
+var point  = document.getElementsByClassName('point');
+var point1  = document.getElementById('point-1');
+var point2  = document.getElementById('point-2');
+var point3  = document.getElementById('point-3');
+var point4  = document.getElementById('point-4');
+var containerBillboard = document.getElementById('container-billboard');
+var billboard = document.getElementById('billboard');
+
+containerBillboard.onmouseleave  = function () {
+  billboard.style.animation = 'slider 10s linear infinite both';
+   for( let i = 0; i < point.length; i++){ /*проходим циклом по всем элементам массивоподобного объекта */
+    point[i].style.background = 'none';  /*удаляю background у кнопок */
+  }
+}
+  
+point1.onclick  = function () {
+  billboard.style.animation = 'slider-1 0.5s linear both';
+  this.style.background = 'blue';
+  point4.style.background = 'none';
+  point2.style.background = 'none';
+  point3.style.background = 'none';
+}
+point2.onclick  = function () {
+  billboard.style.animation = 'slider-2 0.5s linear both';
+  this.style.background = 'blue';
+  point1.style.background = 'none';
+  point3.style.background = 'none';
+  point4.style.background = 'none';
+}
+point3.onclick  = function () {
+  billboard.style.animation = 'slider-3 0.5s linear both';
+  this.style.background = 'blue';
+  point1.style.background = 'none';
+  point2.style.background = 'none';
+  point4.style.background = 'none';
+}
+point4.onclick  = function () {
+  billboard.style.animation = 'slider-4 0.5s linear both';
+  this.style.background = 'blue';
+  point1.style.background = 'none';
+  point3.style.background = 'none';
+  point2.style.background = 'none';
+}
+  
+
+
+/* -----billboard--------- */
+function billboardF() {
+  var heightItem = document.getElementById('item-1').offsetHeight;
+  var distance1 = heightItem * 49 / 100;
+  document.getElementById('item-1').style.transform = 'translateZ(' + distance1 + 'px )';
+  document.getElementById('item-2').style.transform = 'rotateX(90deg)' + 'translateZ(' + distance1 + 'px )';
+  document.getElementById('item-3').style.transform = 'rotateX(180deg)' + 'translateZ(' + distance1 + 'px )';
+  document.getElementById('item-4').style.transform = 'rotateX(270deg)' + 'translateZ(' + distance1 + 'px )';
+  console.log('высота billboard ' + billboard.offsetHeight);
+  console.log('translateZ ' + distance1);
+}
+billboardF();
+
+
+
+
+
 
